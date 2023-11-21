58052137aa239b430a8983c0526abddd573305b6
kristen <kristen346@revature.net>
2023-11-21 13:44:28 -0600

    test 1

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,21 +1,21 @@
 import Image from 'next/image'
 import HeroSection from '../../components/HeroSection'
-import Navbar from '../../components/Navbar'
-import AboutSection from '../../components/AboutSection'
-import ProjectSection from '../../components/ProjectSection'
-import EmailSection from '../../components/EmailSection'
-import Footer from '../../components/Footer'
+// import Navbar from '../../components/Navbar'
+// import AboutSection from '../../components/AboutSection'
+// import ProjectSection from '../../components/ProjectSection'
+// import EmailSection from '../../components/EmailSection'
+// import Footer from '../../components/Footer'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <Navbar />
+      {/* <link rel="icon" href="/favicon.ico" sizes="any" />
+      <Navbar /> */}
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
-        <AboutSection />
+        {/* <AboutSection />
         <ProjectSection />
-        {/* <EmailSection /> */}
+        <EmailSection /> */}
         
      </div>
      <Footer />
