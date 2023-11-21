dc154bc345006e08bbaabafd10d3a9422e106014
kristen <kristen346@revature.net>
2023-11-21 13:47:12 -0600

    test 3

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,24 +1,11 @@
-import Image from 'next/image'
-import HeroSection from '../../components/HeroSection'
-// import Navbar from '../../components/Navbar'
-// import AboutSection from '../../components/AboutSection'
-// import ProjectSection from '../../components/ProjectSection'
-// import EmailSection from '../../components/EmailSection'
-// import Footer from '../../components/Footer'
+import AboutSection from '../../components/AboutSection'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      {/* <link rel="icon" href="/favicon.ico" sizes="any" />
-      <Navbar /> */}
       <div className='container mt-24 mx-auto px-12 py-4'>
-        {/* <HeroSection /> */}
-         <AboutSection />
-        {/*<ProjectSection />
-        <EmailSection /> */}
-        
+        <AboutSection />
      </div>
-     <Footer />
     </main>
   )
 }
