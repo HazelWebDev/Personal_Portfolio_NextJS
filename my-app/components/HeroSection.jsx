010672f9172f2d75f34556d20d769af33c26ac0e
KristenRoggero <kristenkristen05@gmail.com>
2023-12-05 13:26:25 -0600

    added resume info

--- my-app/components/HeroSection.jsx
+++ my-app/components/HeroSection.jsx
@@ -1,58 +1,63 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 lg:grid-cols-12 mt-16'>
-            <div className='col-span-7 place-self-center text-center sm:text-left'>
-                <h1 className='text-pink-400 mb-4 text-2xl sm:text-4xl lg:text-4xl font-extrabold'>
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-pink-400'>
-                        Hello, I&apos;m {""}</span>
-                        <TypeAnimation
-                        sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                        'Kristen Roggero',
-                        1000, // wait 1s before replacing "Mice" with "Hamsters"
-                        'a web developer',
-                        1000,
-                        'an optimist',
-                        1000,
-                        'and a daydreamer',
-                        1000
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={Infinity}
-                        />
-                </h1>
-                <p className='text-pink-200 mb-6 sm:text-lg'>A developer, computer enthusiast, and crochet artist!</p>
-                <div className='mb-6'>
-                    <button className='mb-6 p-2 py-2 px-5 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-primary to-secondary text-dark border-4 border-white font-semibold'>
-                        Contact Me</button>
-                    <button className='p-1 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-secondary to-primary text-primary font-primary'>
-                        <span className='block bg-dark hover:bg-l1 rounded-full px-5 py-2'>
-                            <Link href="/resume">Check out my resume</Link>
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-                <div className='border-4 border-green-200 rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                <Image 
-                className='absolute transform rotate-12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                src="/Portrait.png" 
-                alt="portrait image"
-                width={325}
-                height={325}/>
-                </div>
-            </div>
+      <div className="grid grid-cols-1 lg:grid-cols-12 mt-16">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-pink-400 mb-4 text-2xl sm:text-4xl lg:text-4xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary">
+              Hello, I&apos;m {""}
+            </span>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Kristen Roggero",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "a web developer",
+                1000,
+                "an optimist",
+                1000,
+                "and a daydreamer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-pink-200 mb-6 sm:text-lg">
+            A developer, computer enthusiast, and crochet artist!
+          </p>
+          <div className="mb-6">
+            <button className="mb-6 p-2 py-2 px-5 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-primary to-secondary text-dark border-4 border-white font-semibold">
+              Contact Me
+            </button>
+            <button className="p-1 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-secondary to-primary text-primary font-primary">
+              <span className="block bg-dark hover:bg-l1 rounded-full px-5 py-2">
+                <Link href="/resume">Check out my resume</Link>
+              </span>
+            </button>
+          </div>
         </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="border-4 border-green-200 rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              className="absolute transform rotate-12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              src="/Portrait.png"
+              alt="portrait image"
+              width={325}
+              height={325}
+            />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- my-app/src/app/resume/page.js
+++ my-app/src/app/resume/page.js
@@ -1,9 +1,140 @@
-import Navbar from "../../../components/Navbar";
-
 export default function Page() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <>
+      <div className="container mx-auto p-4 lg:p-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-24">
+          {/* Left Column */}
+          <div className="lg:pr-4">
+            <div className="mb-4">
+              <h1 className="mb-2 text-2xl sm:text-4xl lg:text-4xl font-extrabold">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-secondary to-primary">
+                  Hello, I&apos;m Kristen Roggero!
+                </span>
+              </h1>
+              <h2 className="text-lg lg:text-xl font-semibold text-primary">
+                A Front-End Engineer
+              </h2>
+            </div>
+
+            <div className="mb-4">
+              <h3 className="text-2xl font-semibold mb-2 text-secondary underline">
+                Profile Summary
+              </h3>
+              <p className="font-secondary text-white">
+                A highly motivated web developer with a passion for creating
+                dynamic and user-friendly websites. I am experienced with HTML,
+                CSS, JavaScript, Next.js, and React.js. Committed to delivering
+                high-quality solutions to clients and collaborating with
+                cross-functional teams.
+              </p>
+            </div>
+
+            <div>
+              <h3 className="text-2xl font-semibold mb-2 text-secondary underline underline-primary">
+                Work History
+              </h3>
+              <div className="mb-2">
+                <h4 className="text-lg font-semibold text-l1">
+                  Front-End Engineer at Infosys
+                </h4>
+                <p className="text-white font-secondary">
+                  August 2022 - Present
+                </p>
+                <ul class="list-disc list-inside mt-3 font-secondary">
+                  <li>
+                    Collaborated with the team to adopt and customize Bootstrap
+                    components for consistent and efficient UI development.
+                  </li>
+                  <li>
+                    Developed and implemented responsive web designs, ensuring
+                    optimal user experiences across various devices and screen
+                    sizes by utilizing media queries and flexible grid systems
+                    for desktops, tablets, and mobile devices.
+                  </li>
+                  <li>
+                    Conducted regular code reviews, identifying and rectifying
+                    potential issues to ensure high-quality, maintainable code.
+                  </li>
+                </ul>
+              </div>
+              <div className="mt-3">
+                <h4 className="text-lg font-semibold text-l1">
+                  Coding Bootcamp at Revature
+                </h4>
+                <p className="text-l1 font-secondary">May 2022 - August 2022</p>
+                <ul class="list-disc list-inside mt-3 font-secondary">
+                  <li>
+                    Leveraged expertise in Java, Spring Boot, and some front-end
+                    technologies to deliver end-to-end web applications meeting
+                    business requirements and creative applications.
+                  </li>
+                  <li>
+                    Learned about database management and organization using the
+                    database mySQL.
+                  </li>
+                  <li>
+                    Designed and implemented RESTful APIs using Spring Boot,
+                    facilitating efficient communication between the front-end
+                    and back-end systems, along with database management.
+                  </li>
+                </ul>
+              </div>
+              {/* Add more work history items as needed */}
+            </div>
+          </div>
+
+          {/* Right Column */}
+          <div>
+            <div className="mb-4">
+              <h3 className="text-xl font-semibold mb-2 underline text-primary">
+                Contact Information
+              </h3>
+              <p className="text-primary font-secondary text-l2">
+                <strong>Email:</strong> kristenroggero@gmail.com
+              </p>
+              <p className="text-primary font-secondary text-l2">
+                <strong>Phone:</strong> (341) 777-8729
+              </p>
+              <p className="text-primary font-secondary text-l2">
+                <strong>LinkedIn:</strong> linkedin.com/in/your-profile
+              </p>
+            </div>
+
+            <div>
+              <h3 className="text-xl font-semibold mb-2 text-primary underline">
+                Technical Skills
+              </h3>
+              <ul className="list-disc list-inside font-secondary mb-6">
+                <li>JavaScript (ES6+)</li>
+                <li>React.js</li>
+                <li>Next.js</li>
+                <li>Git Version Control</li>
+                <li>Responsive Web Design</li>
+                <li>Tailwind CSS</li>
+                <li>Bootstrap</li>
+                <li>HTML5</li>
+                <li>CSS</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold mb-2 text-primary underline">
+                Soft Skills
+              </h3>
+              <ul className="list-disc list-inside font-secondary">
+                <li>Problem Solving</li>
+                <li>Time Management</li>
+                <li>Interpersonal Communication</li>
+                <li>Writing</li>
+                <li>Attention to Detail</li>
+                <li>Learning</li>
+                <li>Creativity</li>
+                <li>Critical Thinking</li>
+                <li>Collaboration</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
